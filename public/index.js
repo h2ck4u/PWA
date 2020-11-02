af8b3c5d06ff6ce4b7a5ceb7344f077fda75db74
jaden.js <jaden.js@kakaocorp.com>
2020-11-02 17:07:45 +0900

    코드정리

---
+++ public/index.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    cahes
+      .open(cacheName)
+      .then((cache) => cache.addAll(cachefiles))
+      .then((e) => self.skipWaiting())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  console.log("[Service Worker] Fetched resource " + e.request.url);
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((response) => response || fetch(e.request))
+      .catch((e) =>
+        caches.open(cacheName).then((cache) => cache.match("/offline.html"))
+      )
+  );
+});
--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,22 @@
-const cacheName = "v1";
-const offline = "/offline.html";
+const cacheVersion = "v1";
+const cachefiles = ["/offline.html"];
 
 self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
   e.waitUntil(
     caches
-      .open(cacheName)
-      .then((cache) => cache.addAll(cacheAssets))
+      .open(cacheVersion)
+      .then(cache.addAll(cachefiles))
       .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("fetch", (e) => {
-  console.log("[Service Worker] Fetched resource " + e.request.url);
   e.respondWith(
-    caches.match(offline).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches
+      .match(e.request)
+      .then(reponse || fetch(e.request))
+      .catch(
+        caches.open(cacheVersion).then((cache) => cache.match("offlien.html"))
+      )
   );
 });
